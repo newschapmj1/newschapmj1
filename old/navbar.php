bf91f44ddb5fd7f223bfd5a9e32d5b9006b7ffb2
webmaster <webmaster@redbridgeramblers.org.uk>
2016-09-18 11:42:36 +0100

    1st commit

---
+++ old/navbar.php
@@ -0,0 +1,73 @@
+<?php
+?>
+        
+<nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+	 <a class="navbar-brand" href="index.php">Home</a>
+   </div>
+
+   <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+    <!-- <li><a href="index.php">Home</a>  -->
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">About us<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+    	    	<li><a href="p/about.php">About Us -root</a></li>
+            <li><a href="p/nonmembers.php">Information for non members</a></li>
+            <li><a href="p/join.php">How to join</a></li>
+            <li><a href="p/history.php">How we got started</a></li>
+            <li><a href="p/websiteinfo.php">Website information</a></li>
+        </ul>
+     </li>
+      <li><a href="p/walks.php">Walks</a></li>
+      <li><a href="walksmap.php">Walks Map</a></li>
+      <li><a href="social.php">Social Events</a></li>
+
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Members<b class="caret"></b></a>
+      <ul class="dropdown-menu">
+       <li><a href="callback.php">Sign in</a></li>
+       <li><a href="logout.php">Sign out</a></li>
+       <li><a href="newsletters.php">Newsletters</a></li>
+        <li><a href="memberpage.php">page with no content</a></li>
+         </ul>
+           </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Photos
+                            <b class="caret"></b></a>
+                    	<ul class="dropdown-menu">
+                          <li><a href="http://www.flickr.com/photos/redbridgerambler/sets/">Steven Muster (Flickr)</a></li>
+                          <li><a href="https://www.flickr.com/gp/132833839@N02/24FLMW/">Isle of Wight April 2015 I (Flickr)</a></li>
+                          <li><a href="https://www.flickr.com/gp/132833839@N02/3Rchk0/">Isle of Wight April 2015 II (Flickr)</a></li>
+                          <li><a href="https://www.flickr.com/gp/132833839@N02/3oRmV8/">Walk Wansted Park 18th April 2015 (Flickr)</a></li>
+                          <li><a href="https://www.flickr.com/gp/132833839@N02/6PY1U5/">Walk Woolwich Arsenal to Abbey Wood - 26th April 2015 (Flickr)</a></li>
+                          <li><a href="https://www.flickr.com/gp/132833839@N02/vQYH1t/">Walk Strethall to Saffron Walden - 16th June 2015 (Flickr)</a></li>
+                          <li><a href="https://www.flickr.com/gp/132833839@N02/zK674q/">Walk/pre-walk Cutty Sark to London Eye- 25th July 2015 (Flickr)</a></li>  
+                          <li><a href="https://www.flickr.com/gp/132833839@N02/25m2a1/">Northumberland July 2015 (Flickr)</a></li>      
+                          <li><a href="https://www.flickr.com/gp/132833839@N02/o4h36D">Harold&lsquo;s Way Oct 2015 (Flickr)</a></li>                            
+                          <li><a href="gallery_2014_spring.php">Spring 2014</a></li>
+                          <li><a href="gallery_2013_backpacking.php">Backpacking May/June 2013</a></li>
+                          <li><a href="gallery_2013_sherborne.php">Sherborne April 2013</a></li>
+                          <li><a href="gallery_2011_misc.php">Miscellaneous 2011 &amp; 2012</a></li>
+                          <li><a href="gallery_2011_iow.php">Isle of Wight April 2011</a></li>
+                          <li><a href="gallery_2011_tring.php">Tring March 2011</a></li>
+                          <li><a href="gallery_2010_valencia.php">Valencia 2010</a></li>
+                          <li><a href="gallery_2010_worthing.php">Worthing 2010</a></li>
+                          <li><a href="gallery_2009_peak.php">Peak District Sep 2009</a></li>
+                       	</ul>
+                    </li>
+                    <li><a href="p/other.php">Other</a></li>
+                    <li><a href="p/ulinks.php">Useful Links</a></li>
+                   </ul> 
+               </div>
+             </nav>
+        
+<?php
+?>
---
+++ old/social.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html manifest="example.appcache">
+<head>
+ <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <!-- <link rel="stylesheet" href="bootstrap/3.3.1/css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="bootstrap/3.3.5/css/bootstrap.min.css" type="text/css"/><link rel="stylesheet" href="css/custom002_jan15.css" type="text/css"/>
+
+
+   <!-- <link href="css/bootstrap.min.css" rel="stylesheet" media="screen"> -->
+
+<script src="js/jquery/jquery-1.11.3.min.js"></script> 
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+    <script src="bootstrap/3.3.5/js/bootstrap.min.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="/favicon.ico"/>
+<!-- <link href='%2F%2Ffonts.googleapis.com%2Fcss%3Ffamily%3DMerriweather%3A400%2C900%7COpen%2BSans' rel='stylesheet' type='text/css'> -->
+<link href='//fonts.googleapis.com/css?family=Merriweather:400,900|Open+Sans' rel='stylesheet' type='text/css'>    
+
+
+<title>Redbridge Ramblers, Social</title>
+
+
+</head>
+<body>
+<?php
+include_once 'analyticstracking.php';
+?>
+<?php
+include 'navbar.php';
+?>
+
+<div id="logo003">
+    <span class="redcolor text-center">R</span>edbridge <span class="redcolor">R</span>amblers
+ </div>
+
+
+                
+<div class="text-center">
+       <h2><b>Social Events Programme</b></h2>
+<div class="container">
+ <div class="row">
+    <div class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12  text-center">
+    <p>Last update: March 14th 2016</p>
+
+
+
+    <h3><strong>Long distance path <a href=
+"https://www.ldwa.org.uk/ldp/members/show_path.php?path_name=Harcamlow+Way">Harcamlow Way</a></strong></h3>
+
+<p  class="text-left">The next 4 legs are on 24 Apr, 22 May, 26 Jun & 17 Jul.  Further 
+details are in the Apr to Jul <a href="walks.php">walks programme</a> and the newsletter.</p>
+
+  <img src="images/latchfordford.jpg" alt="Harcamlow Way - Latchford Ford " class="img-responsive"   />
+<figcaption>Latchford Ford</figcaption>
+<br>
+<h3>Wealden Round – 2016 Joint coach walk with West Essex</h3>
+
+<p class="text-left">This new 50 mile circular route through many attractive villages in the Kent Weald will be walked over five Sundays. </p>
+
+<ul class="text-left">
+They are :
+<li>May 15th Tonbridge to Cowden about 11.5 miles</li>
+<li>June 12th Cowden to Crockham Hill about 11 miles</li>
+<li>July 3rd  Crockham to Sevenoaks about 9 miles</li>
+<li>Sept 4th Sevenoaks to West Peckham about 10 miles</li>
+<li>Oct 23rd West Peckham to Tonbridge about 10 miles</li>
+</ul>
+<p class="text-left">It is hoped that all legs will have a pub stop but please bring a packed lunch.</p>
+
+<p class="text-left">As all members know the cost of hiring coaches is very expensive, so we ask you to book as soon as possible. </p>
+
+<p class="text-left">May , June and July legs bookable now.</p>
+
+<p class="text-left">Please complete booking slips on Pages 10 & 11 and return with Cheque (£14.50 per person) made payable to West Essex Ramblers</p>
+<br>
+
+<h3><strong><a href="http://www.nationaltrail.co.uk/offas-dyke-path">Offa's Dyke Backpacking</a> – May 28th – June 5 2016</strong></h3>
+
+<p class="text-left">The backpacking holiday for 2016 will be the first half (well just under) of Offa’s Dyke National Trail. We will walk from Sedbury Cliffs (just outside Chepstow) to Knighton.<p>
+<p class="text-left">The total length to be walked will be about 90 miles including walking off the path for accommodation. Walking distances will be between 10 and 14.5 miles and we will stay in B&Bs along the route.</p>
+<p class="text-left">Although B&B bookings have been made, it may still be possible to join the holiday. Please contact Roger.</p>
+  <img src="images/offasdyke.jpg" alt="view from Rushock Hill " class="img-responsive"   />
+<figcaption>Offa's Dyke, Looking west from Rushock Hill</figcaption>
+
+<br>
+<h3><strong>Coach Assisted Walking Holiday 26th-30th July 2016</strong></h3>
+
+
+<p class="text-left">This year’s coach assisted walking holiday will be The <a href="https://www.ldwa.org.uk/ldp/members/show_path.php?path_name=Limestone+Way">Limestone Way</a>. This is an easy 46 mile route from 
+Rocester, North Staffordshire to Castleton in Derbyshire following many dales, tracks and lanes through 
+magnificent limestone country. </p>
+
+<p class="text-left">The larger part of the group will be staying at Hathersage Youth, although some have booked B&B accommodation in the village. It may still be possible to book for this holiday – contact Roger if interested. Those who have booked will be contacted in March as to the balance payable.</p>
+
+  <img src="images/limestoneway.jpg" alt="Limestone Way " class="img-responsive"   />
+<figcaption>Limestone Way</figcaption>
+<br>
+<br>
+<h2><b>Recent Activies</b></h2>
+
+<h3>ANNUAL QUIZ NIGHT</h3>
+<h3>Saturday 12 March 2016 at 7.30pm</h3>
+
+<p>at St. George’s Church Hall, Gants Hill (corner of Woodford Avenue / Gants Hill Crescent)</p>
+
+<p>Come on your own or bring your friends. Tables of 8 or individual tickets and make up a team on the night. </p>
+
+<p>An excellent ploughman’s supper will be provided. Bring your own drinks. </p>
+<p>Tickets are £8.50 per person</p>
+
+</div>      
+</div>        
+</div>      
+</div>      
+
+
+</body>
+</html>
